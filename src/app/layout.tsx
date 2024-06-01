c97c8998e6fbb47812592dd7746bc985b8c2f017
Harshana_P_Semasinghe <harshanaprabhathsemasinghe@gmail.com>
2024-06-02 00:13:09 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/Utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        'min-h-screen font-sans antialiased grainy dark:bg-slate-900',inter.className
+      )}>{children}</body>
     </html>
   );
 }
